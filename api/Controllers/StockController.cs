b1422b564edf93188a5b249b2f4a7b85ce9b8ec7
fangimal <67121530+fangimal@users.noreply.github.com>
2024-10-10 14:27:05 +0500

    added stock dto

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,38 @@
+using api.Data;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[Route("api/stock")]
+[ApiController]
+public class StockController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    public StockController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var stock = _context.Stocks.ToList()
+            .Select(s => s.ToStockDto());
+        return Ok(stock);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(
+        [FromRoute] int id)
+    {
+        var stock = _context.Stocks.Find(id);
+
+        if (stock == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(stock.ToStockDto());
+    }
+}
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47880c4ce0c8584124a713cdce0aca33cf5d88e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebdeda42253fd71071a029cd0c6657febebbc8b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
