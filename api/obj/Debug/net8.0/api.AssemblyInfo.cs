f055ef5631584640e51d1365357a003c107cb3c8
fangimal <gimalfan@gmail.com>
2024-10-15 23:12:22 +0500

    Portfolio Get
    Signed-off-by: fangimal <gimalfan@gmail.com>

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0e0d2448b022eddfceb30e695f2b34c879e2833")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c7edbc783c5d0c693ed201fa38e162a6cfb7ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
